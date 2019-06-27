8e5b6b40fe3c0c35156a8da1d356ebc23e39fe30
Jen-Soft <jen.soft.master@gmail.com>
2019-06-27 13:06:33 -0400

    set mariadb

--- src/app_django/settings.py
+++ src/app_django/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'app_django.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':     'unti_cards',
+        'USER':     'root',
+        'PASSWORD': '7895123qw',
+        'HOST':     '127.0.0.1',   # Or an IP Address that your DB is hosted on
+        'PORT':     '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
