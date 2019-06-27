26c73ab422892092b8d831ad8624825b561c9c04
Jen-Soft <jen.soft.master@gmail.com>
2019-06-27 10:26:17 -0400

    add django-app: red_cards

--- src/app_django/settings.py
+++ src/app_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cgju4#-d_ewl!vt$t=!hh+0!jpqk+teyo$kwl1v(6$boob2st='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'red_cards',
 ]
 
 MIDDLEWARE = [
