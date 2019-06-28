09dd93d4b5770ba3231c55f0bc388a4d0ba37b10
Jen-Soft <jen.soft.master@gmail.com>
2019-06-28 04:45:15 -0400

    fix: roll back db settings to mysql;

--- src/app_django/settings.py
+++ src/app_django/settings.py
@@ -79,23 +79,23 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv("MYSQL_DATABASE"),
-#         'USER': os.getenv("MYSQL_USER"),
-#         'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-#         'HOST': os.getenv("DB_NAME"),
-#         'PORT': 3306,
-#         'OPTIONS': {'charset': 'utf8'},
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("MYSQL_DATABASE"),
+        'USER': os.getenv("MYSQL_USER"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': os.getenv("DB_NAME"),
+        'PORT': 3306,
+        'OPTIONS': {'charset': 'utf8'},
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
