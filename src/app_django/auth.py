8a6dae95ff7a8ba069ca3a8826a2611097d5a254
Jen-Soft <jen.soft.master@gmail.com>
2019-07-02 05:57:04 -0400

    hot-fix: oauth: pipline module name app_django.update_user

--- src/app_django/auth.py
+++ src/app_django/auth.py
@@ -36,7 +36,7 @@ class UNTIBackend(BaseOAuth2):
         'social_core.pipeline.social_auth.auth_allowed',
         'social_core.pipeline.social_auth.social_user',
         'social_core.pipeline.user.create_user',
-        'app_django.update_user',
+        'app_django.auth.update_user',
         'social_core.pipeline.social_auth.associate_user',
         'social_core.pipeline.social_auth.load_extra_data',
         'social_core.pipeline.user.user_details',
